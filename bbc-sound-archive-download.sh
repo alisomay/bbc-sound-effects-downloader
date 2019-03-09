bdb7a7c5f975f38f0249adc1199083deb35c010a
alisomay <alisomay@gmail.com>
2019-03-09 15:12:55 +0100

    prettify and remove unnecessary comments

--- bbc-sound-archive-download.sh
+++ bbc-sound-archive-download.sh
@@ -8,29 +8,30 @@ downloadpath=$(echo $1 | tr -s /);
 	while IFS=, read -r url
 	do
 
-	    url=$(echo "$url" | tr -d ',');
-	    if [[ $url =~ $re ]]; then filename=$(echo ${BASH_REMATCH[1]}); fi
-	    # echo $filename;
-	    if [ $(find $downloadpath -name $filename 2>/dev/null | wc -l) -gt 0 ]
-			then
-			   echo -ne "Finding the right index to resume download, please be patient.\r";
-			else
-	    
-	        wget -q --show-progress $downloadpath $url; 
+		url=$(echo "$url" | tr -d ',');
+		if [[ $url =~ $re ]]; then filename=$(echo ${BASH_REMATCH[1]}); fi
 
-     	    if (( $? != 0 ));
-				then
-				  echo -ne " Trying, "$url" , no file there, please be patient.\r"
-				else
-				  echo "Found file "$url" and downloading the file to the specified location."
-			fi
-	
+		if [ $(find $downloadpath -name $filename 2>/dev/null | wc -l) -gt 0 ]
+			then
+				echo -ne "Finding the right index to resume download, please be patient.\r";
+			else
+		
+				wget -q --show-progress $downloadpath $url; 
+
+				if (( $? != 0 ));
+					then
+						echo -ne " Trying, "$url" , no file there, please be patient.\r"
+					else
+						echo "Found file "$url" and downloading the file to the specified location."
+				fi
+		
 		fi
 	
 	done < $filepath
 
+
 count=$(ls -l "$1" | grep -v ^l | wc -l);
-echo "The script downloaded"$count" files."
+echo "The script has downloaded"$count" files."
 
 
 
