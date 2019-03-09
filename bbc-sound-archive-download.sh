17986a44381c9c4aa2fe8a5d1fab1af3973916dc
alisomay <alisomay@gmail.com>
2019-03-09 14:47:53 +0100

    renew script, add resume function

--- bbc-sound-archive-download.sh
+++ bbc-sound-archive-download.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
 filepath="./urls-to-download.csv"
+re="([0-9]{8}\.wav)"
+downloadpath=$(echo $1 | tr -s /);
+
 	
 	while IFS=, read -r url
 	do
-	    echo "$url" | tr -d ',';
-	    wget_out=$(wget -q -P $1 "$url"); 
-		    
-		    if (( $? != 0 ));
+	    url=$(echo "$url" | tr -d ',');
+	    if [[ $url =~ $re ]]; then filename=$(echo ${BASH_REMATCH[1]}); fi
+	    # echo $filename;
+	    if [ $(find $downloadpath -name $filename 2>/dev/null | wc -l) -gt 0 ]
+			then
+			   echo -ne "Finding the right index to resume download, please be patient.\r";
+			else
+	    
+	        wget -q --show-progress $downloadpath $url; 
+
+     	    if (( $? != 0 ));
 				then
 				  echo -ne " Trying, "$url" , no file there, please be patient.\r"
 				else
-				  echo "Found file "$url" and downloaded the file to the specified location."
+				  echo "Found file "$url" and downloading the file to the specified location."
 			fi
+	
+		fi
 	
 	done < $filepath
 
 count=$(ls -l "$1" | grep -v ^l | wc -l);
 echo "The script downloaded"$count" files."
+
 
 
