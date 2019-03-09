3b09a4f8a523362fd0f6c75b90f3833353b3f58d
alisomay <alisomay@gmail.com>
2019-03-09 11:15:33 +0100

    prettify script

--- bbc-sound-archive-download.sh
+++ bbc-sound-archive-download.sh
@@ -4,14 +4,16 @@ filepath="./urls-to-download.csv"
 	
 	while IFS=, read -r url
 	do
-	      echo "$url" | tr -d ',';
-	      wget_out=$(wget -q -P $1 "$url"); 
-	      if (( $? != 0 ));
-			  then
-			    echo -ne " Trying, "$url" , no file there, please be patient.\r"
-			  else
-			    echo "Found file "$url" and downloaded the file to the specified location."
-		  fi
+	    echo "$url" | tr -d ',';
+	    wget_out=$(wget -q -P $1 "$url"); 
+		    
+		    if (( $? != 0 ));
+				then
+				  echo -ne " Trying, "$url" , no file there, please be patient.\r"
+				else
+				  echo "Found file "$url" and downloaded the file to the specified location."
+			fi
+	
 	done < $filepath
 
 count=$(ls -l "$1" | grep -v ^l | wc -l);
